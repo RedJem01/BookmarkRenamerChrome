202c714ef84741fa426f4f3f7f0a4bbbac338241
Jemima <jemstobart@hotmail.com>
2025-07-14 01:14:26 +0100

    Add extension files for renaming bookmarks

---
+++ background.ts
@@ -0,0 +1,18 @@
+chrome.bookmarks.onCreated.addListener(async(id: string, bookmark: chrome.bookmarks.BookmarkTreeNode) => {
+    if (!bookmark.unmodifiable && !bookmark.folderType) {
+        const url = bookmark.url!;
+        const urlSplit = url.split("/");
+        const domain = urlSplit[2];
+        const domainSplit = domain.split(".");
+        let webName = domainSplit[0];
+        if (domainSplit[0] == "www") {
+            webName = domainSplit[1];
+        }
+        const upperWebName = webName.charAt(0).toUpperCase() + webName.slice(1)
+        console.log(upperWebName);
+        // chrome.bookmarks.update(bookmark.id, {title: upperWebName});
+    }
+    else {
+        console.log("Either this bookmark is unmodifiable or is a folder.");
+    }
+});
